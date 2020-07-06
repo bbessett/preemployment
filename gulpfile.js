58659a38acadff4d677a4d79334182efc39f188a
Brett <bbesset@transunion.com>
2020-07-06 16:11:58 -0600

    Nanny Page

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 // var livereload = require('gulp-livereload');
 // var sourcemaps = require('gulp-sourcemaps');
 
@@ -12,11 +13,33 @@ var sass = require('gulp-sass');
 //     .pipe(sourcemaps.write())
 // });
 
-gulp.task('sass', function (done) {
+var input = 'scss/**/*.scss';
+var output = 'css';
+
+
+gulp.task('prod', function (done) {
 	gulp.src('scss/**/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(sass({outputStyle: 'compressed'}))
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest(output))
+  done();
+ });
+
+ var sassOptions = {
+	errLogToConsole: true,
+	outputStyle: 'expanded'
+}; 
+
+gulp.task('sass', function (done) {
+	gulp.src(input)
+	.pipe(sourcemaps.init())
+	.pipe(sass(sassOptions).on('error', sass.logError))
+	.pipe(sourcemaps.write())
+	  //   .pipe(autoprefixer({
+    //         browsers: ['last 99 versions'],
+    //         cascade: false
+    // }))
+  .pipe(gulp.dest(output))
   done();
  });
 
@@ -27,7 +50,7 @@ gulp.task('clean', function() {
 
 gulp.task('watch', () => {
   // livereload.listen(); 
-  gulp.watch('scss/**/*.scss', (done) => {
+  gulp.watch(input, (done) => {
       gulp.series(['sass'])(done);
   });
 });
